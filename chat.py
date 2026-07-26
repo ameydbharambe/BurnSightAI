c83d25f6c121c8b57538bacaab96e85c1565da3d
Amey <ameybharambe@gmail.com>
2026-07-26 13:04:50 -0700

    created prompt.py for easier readability

--- chat.py
+++ chat.py
@@ -2,6 +2,7 @@ from google import genai
 from dotenv import load_dotenv
 import os
 from model import BurnClassifier
+import prompt
 
 load_dotenv()
 
@@ -14,26 +15,19 @@ class BurnSightChat:
     # ---------------------------------------------------------------------------
     
     def diagnose(self, prediction, confidence):
-        prompt = """You are a FIRST AID medical assistant specializing in the treatment of burn injuries (First Degree, Second Degree, Third Degree). 
-        A user has uploaded an image of a burn injury, and the model has classified it 
-        as"""+ prediction + """ with a confidence of""" + str(confidence) + """. If the confidence is below 0.5, please advise the user to seek professional medical attention immediately.
-        Please provide a short yet detailed and informative response (200 words maximum) based on the classification and confidence level
-        for treatment. Only suggest immediate diagnosis and treatment options that are easily accessible to the user at any given location and be done before calling medical assistance. 
-        Avoid suggesting any treatment that requires a hospital visit or prescription medication."""
+        prompt_text = prompt.diagnosis_prompt(prediction, confidence)
         
         response = self.client.models.generate_content(
             model="gemini-2.5-flash",
-            contents=[prompt],
+            contents=[prompt_text],
         )
         return response.text
     
     def chat(self, question):
-        prompt = """You are a FIRST AID medical assistant specializing in treating burn injuries (First Degree, Second Degree, Third Degree)
-        The user has already uploaded an image of a burn injury and received a diagnosis. The user now has the following follow up question related to burn injuries: """ + question + """ Please provide a short yet detailed and informative response (200 words maximum) based on the user's question. 
-        DO NOT answer any questions that are not related to burn injuries. If the question is not related to burn injuries, please politely inform the user that you can only answer questions related to burn injuries."""
+        prompt_text = prompt.chat_prompt(question)
         response = self.client.models.generate_content(
             model="gemini-2.5-flash",
-            contents=[prompt],
+            contents=[prompt_text],
         )
         return response.text
     
@@ -46,6 +40,8 @@ print(diagnosis)
 follow_up_question = input()
 follow_up_response = chatbot.chat(follow_up_question)
 print(follow_up_response)
+
+
 
 
 
---
+++ prompt.py
@@ -0,0 +1,14 @@
+def diagnosis_prompt(prediction, confidence):
+    prompt = """You are a FIRST AID medical assistant specializing in the treatment of burn injuries (First Degree, Second Degree, Third Degree). 
+        A user has uploaded an image of a burn injury, and the model has classified it 
+        as"""+ prediction + """ with a confidence of""" + str(confidence) + """. If the confidence is below 0.5, please advise the user to seek professional medical attention immediately.
+        Please provide a short yet detailed and informative response (200 words maximum) based on the classification and confidence level
+        for treatment. Only suggest immediate diagnosis and treatment options that are easily accessible to the user at any given location and be done before calling medical assistance. 
+        Avoid suggesting any treatment that requires a hospital visit or prescription medication."""
+    return prompt
+
+def chat_prompt(question):
+    prompt = """You are a FIRST AID medical assistant specializing in treating burn injuries (First Degree, Second Degree, Third Degree)
+        The user has already uploaded an image of a burn injury and received a diagnosis. The user now has the following follow up question related to burn injuries: """ + question + """ Please provide a short yet detailed and informative response (200 words maximum) based on the user's question. 
+        DO NOT answer any questions that are not related to burn injuries. If the question is not related to burn injuries, please politely inform the user that you can only answer questions related to burn injuries. When answering, please recall previous information about the burn injury diagnosis and treatment options provided to the user. Avoid suggesting any treatment that requires a hospital visit or prescription medication."""
+    return prompt
