01d4d885d8992fef953a5931d952f73f9d7933c7
Amey <ameybharambe@gmail.com>
2026-07-28 22:48:49 -0700

    Created a basic intro page.

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 import streamlit as st
 import requests
 
-#DEFAULT FONT = ROBOTO TODO: POTENTIAL CHANGE TO OPEN SANS
-
 st.markdown("""
 <style>
+    .block-container {
+        padding-top:3.0rem;
+    }
 
-/* Change font for entire application */
-html, body, [class*="css"], [data-testid="stAppViewContainer"] * {
-    font-family: "Comic Sans MS" !important;
-}
+    h1 {
+        margin-top: -2px;
+        margin-bottom: 2px;
+    }
 
+    p {
+        margin-top: 0px;
+    }
 </style>
 """, unsafe_allow_html=True)
 
-#TITLE & SUBHEADER
-st.markdown( """ <h1 style="text-align:center; color:#D32F2F;">BurnSight AI</h1>""",unsafe_allow_html=True)
-st.write("Welcome to BurnSightAI! To begin your diagnosis please upload an image of the burn injury. Once the diagnosis is made, you can ask follow-up questions related to immediate treatment for the injury.")
+
+# Center smaller logo
+col1, col2, col3 = st.columns([1.25,1, 1.25])
+with col2:
+    st.image("FrontEnd/BurnSightAI Logo.png", width=700)
+
+
+st.markdown(
+    "<h1 style='text-align:center; color:#D32F2F;'>BurnSight AI</h1>",
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <p style='text-align:center; font-size:18px;'>
+    Please begin by uploading an image of the burn.
+    </p>
+    """,
+    unsafe_allow_html=True
+)
