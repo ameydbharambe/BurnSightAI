90fe7cd3e5016c87f558b67e37b50e4aa2506c81
Amey <ameybharambe@gmail.com>
2026-07-28 21:26:16 -0700

    Testing out new font and color scheme

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import requests
 
-st.title("BurnSightAI")
+#DEFAULT FONT = ROBOTO TODO: POTENTIAL CHANGE TO OPEN SANS
+st.markdown("""
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600;700&display=swap');
+
+html, body, [class*="css"]  {
+    font-family: 'Roboto', sans-serif;
+}
+</style>
+""", unsafe_allow_html=True)
+
+#TITLE & SUBHEADER
+st.markdown( """ <h1 style="text-align:center; color:#D32F2F;">BurnSight AI</h1>""",unsafe_allow_html=True)
 st.write("Welcome to BurnSightAI! To begin your diagnosis please upload an image of the burn injury. Once the diagnosis is made, you can ask follow-up questions related to immediate treatment for the injury.")
