62a757d39d7f88746741210b6dc15bf7446b9012
Amey <ameybharambe@gmail.com>
2026-08-08 03:17:05 -0700

    added new model to gitignore and updated model to use mobilenet instead of resnet

--- model.py
+++ model.py
@@ -1,26 +1,24 @@
+from xml.parsers.expat import model
+
 import torch
 import torch.nn as nn
-
+from torchvision import transforms, models
+from torchvision.models import ResNet50_Weights
 from PIL import Image
 
-from torchvision import transforms
-
-from transformers import ResNetForImageClassification
-
 class BurnClassifier:
-    def __init__(self):
-        
+    def __init__(self, model_path="BurnClassifierMOBILENET.pt", num_labels=3):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = self.load_model(model_path, num_labels)
         
-        #Class names in order of dataset folder labels
+        #Order of class names must match order of file labels in dataset folder
         self.class_names = [
             "First degree burn",
             "Third degree burn",
             "Second degree burn",
         ]
-        self.outputs = len(self.class_names)
-        
-        #Apply required transformations to process images for ResNet50 model
+
+        #Standard image normalization for MobileNetV3 model
         self.transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
@@ -29,27 +27,19 @@ class BurnClassifier:
                 std=[0.229, 0.224, 0.225]
             )
         ])
-        
-        self.model = self.load_model()
-        
-    def load_model(self):
-        
-        model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+
+    #Swap classification head with 3-class custom head
+    def load_model(self, model_path, num_labels):
+        model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
         model.classifier = nn.Sequential(
-        nn.Flatten(1),
-        nn.Linear(2048, 128),
-        nn.ReLU(inplace=True),
-        nn.Linear(128, self.outputs)
-        )
-        
-        state_dict = torch.load("BurnClassifierTransformer.pth", map_location=self.device)
-        model.load_state_dict(state_dict)
-
+                    nn.Linear(576, 1000, bias=True),
+                    nn.Hardswish(),
+                    nn.Dropout(p=0.3, inplace=True),
+                    nn.Linear(1000, num_labels))
         model.to(self.device)
+        model.load_state_dict(torch.load(model_path, map_location=self.device))
         model.eval()
-
         return model
-    
     
     def predict(self, image_path):
         image = Image.open(image_path).convert('RGB')
@@ -57,8 +47,8 @@ class BurnClassifier:
         image = image.unsqueeze(0).to(self.device)
         
         with torch.no_grad():
-            outputs = self.model(pixel_values=image)
-            probabilities = torch.softmax(outputs.logits, dim=1)
+            outputs = self.model(image)
+            probabilities = torch.softmax(outputs, dim=1)
             confidence, predicted = torch.max(probabilities, 1)
 
         return self.class_names[predicted.item()],  confidence.item()
